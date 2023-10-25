f396a9f4b783e1f08c2a9aabc73029778739b3fe
Jylant <t2jyan00@students.oamk.fi>
2023-10-25 12:12:34 +0300

    just stuff

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log("DOM loaded");
     const button = document.getElementById('nappi'); // select the button element
     const navButton = document.getElementById('nav-toggle'); // select the button element
-    const navItems = document.getElementsByClassName('nav-item');
 
     button.addEventListener('click', handleClick);
     navButton.addEventListener('click', handleClick);
-    for (const item in navItems) {
-        item.addEventListener('click', handleClick);
-    }
     console.log("Listeners added");
 
     function handleClick(Event) {
@@ -18,39 +14,24 @@ document.addEventListener('DOMContentLoaded', function() {
             button.textContent = "Javascript says hello, too!";
         } else if (Event.target.class == "nav-item") {
             console.log("nav-item clicked");
-            const xhr = new XMLHttpRequest();
-            const linkki = document.getElementById('linkki');
-            console.log(Event.target.innertext);
+            const linkki = document.getElementById('ruutu').contentWindow.document.getElementById('kuva');
             switch (Event.target.innertext) {
                 case "Home":
-                    xhr.open('GET', '/scripts/kuu.txt', true);
-                    linkki.setAttribute("href", "#");
+                    linkki.src="favicon.ico";
                     break;
                 case "About":
-                    xhr.open('GET', './about.html', true);
-                    linkki.setAttribute("href", "https://github.com/Jylant");
+                    linkki.src="Moon.jpg";
                     break;
                 case "Contact":
-                    xhr.open('GET', './contact.html', true);
                     linkki.setAttribute("href", "https://github.com/Jylant");
                     break;
                 case "Github":
-                    xhr.open('GET', 'scripts/hedron.txt', true);
                     linkki.setAttribute("href", "https://github.com/Jylant");
                     break;
                 default:
                     console.log("No match");
                     break;
             }
-            
-            xhr.onload = function() {
-                if (this.status === 200) {
-                    const newContent = this.responseText;
-                    const elementToReplace = document.getElementById('linkkikuva');
-                    elementToReplace.setAttribute("src", newContent);
-                }
-            }
-            xhr.send();
         }
         console.log(Event.target.nodeName);
     }
